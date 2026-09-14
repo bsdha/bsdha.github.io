06aff415777d5e53f710633ec0093dfb770a684e
Guitar72 <dhabolero@gmail.com>
2026-09-14 21:23:14 +0700

    Add files via upload

--- helix/helix_utilities.user.js
+++ helix/helix_utilities.user.js
@@ -6,7 +6,7 @@
 // @match        https://his.benhvienbinhduong.org.vn/*
 // @run-at       document-idle
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/bsdha/bsdha.github.io/refs/heads/main/helix/helix_utilities.user.js
+// @updateURL    https://raw.githubusercontent.com/bsdha/bsdha.github.io/refs/heads/main/helix/helix_utilities.meta.js
 // @downloadURL  https://raw.githubusercontent.com/bsdha/bsdha.github.io/refs/heads/main/helix/helix_utilities.user.js
 // ==/UserScript==
 
