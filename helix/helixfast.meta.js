a1169851aaf94f468c5de7276adaab8425a4d01e
Guitar72 <dhabolero@gmail.com>
2026-09-15 10:38:01 +0700

    Add files via upload

---
+++ helix/helixfast.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         HIS Bình Dương - Tiện ích Helix
+// @namespace    https://his.benhvienbinhduong.org.vn/
+// @version      2.0
+// @description  Tiện ích Helix
+// @match        https://his.benhvienbinhduong.org.vn/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/bsdha/bsdha.github.io/refs/heads/main/helix/helixfast.meta.js
+// @downloadURL  https://raw.githubusercontent.com/bsdha/bsdha.github.io/refs/heads/main/helix/helixfast.user.js
+// ==/UserScript==
