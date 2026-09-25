1953dc7672da66a71afc1613b922ad9a1b7bb178
Guitar72 <dhabolero@gmail.com>
2026-09-25 16:41:18 +0700

    Update helixfast.meta.js

--- helix/helixfast.meta.js
+++ helix/helixfast.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Helixfast
 // @namespace    https://his.benhvienbinhduong.org.vn/
-// @version      1.74
+// @version      1.76
 // @description  Tiện ích Helix
 // @match        https://his.benhvienbinhduong.org.vn/*
 // @run-at       document-idle
